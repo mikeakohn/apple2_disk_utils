b8e70e11a0d15c22b7b01161ee152dc03618d084
Michael Kohn <mike@mikekohn.net>
2015-08-23 17:39:26 -0500

    Rename read_disk_ii to read_dos33_disk.

--- read_dos33_disk.go
+++ read_dos33_disk.go
@@ -5,7 +5,7 @@ import "os"
 import "apple2_disk"
 
 func main() {
-  fmt.Println("read_disk_ii: Apple II disk reader - Copyright 2015 Michael Kohn")
+  fmt.Println("read_dos33_disk: Apple II disk reader - Copyright 2015 Michael Kohn")
 
   if len(os.Args) < 2 {
     fmt.Println("Usage: " + os.Args[0] + " <binfile> <optional:file to dump> <optional:outputfile>")
